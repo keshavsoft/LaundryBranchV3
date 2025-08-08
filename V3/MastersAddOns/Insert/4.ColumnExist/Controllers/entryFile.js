17e19a4780a86c2840e54d1b8211a3a6c28d3482
keshavsoft <nknnkumar@yahoo.com>
2025-08-08 18:13:37 +0530

    V3 perfect with 2 tables

---
+++ V3/MastersAddOns/Insert/4.ColumnExist/Controllers/entryFile.js
@@ -0,0 +1,23 @@
+import {
+    postDefaultFunc as postDefaultFuncFromRepo
+} from '../Repos/entryFile.js';
+
+let postFilterDataFromBodyFunc = (req, res) => {
+    let LocalRequestBody = req.body;
+	let LocalCoumnAddOnRate = LocalRequestBody.AddOnRate;
+	let LocalCoumnAddOnService = LocalRequestBody.AddOnService;
+
+    let LocalFromRepo = postDefaultFuncFromRepo({LocalCoumnAddOnService,LocalCoumnAddOnRate});
+
+    if (LocalFromRepo.KTF === false) {
+        res.status(409).send(LocalFromRepo.KReason);
+        return;
+    };
+
+    res.set('Content-Type', 'text/plain');
+    res.send(LocalFromRepo.SuccessText);
+};
+
+export {
+    postFilterDataFromBodyFunc
+};
